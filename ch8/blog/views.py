2f7971075435bf7338ad74940b47664c1f907d15
Lee Jin Woo <jwoo619@naver.com>
2017-11-16 15:38:10 +0900

    ch8
    startproject ch8

---
+++ ch8/blog/views.py
@@ -0,0 +1,44 @@
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
+
+from blog.models import Post
+from tagging.models import Tag, TaggedItem
+from tagging.views import TaggedObjectList
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+class PostDV(DetailView):
+    model = Post
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_date'
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field= 'modify_date'
+    make_object_list = True
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+class TagTV(TemplateView):
+    template_name = 'tagging/tagging_cloud.html'
+
+class PostTOL(TaggedObjectList):
+    model = Post
+    template_name = 'tagging/tagging_post_list.html'

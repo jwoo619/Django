dd3cdb5a247b15dc696750592d87aea32e4d4ee9
Lee Jin Woo <jwoo619@naver.com>
2017-11-20 23:46:00 +0900

    new app photo
    fix
    mysite/setting.py
    photo/(admin.py , fields.py models.py)

--- ch10/mysite/settings.py
+++ ch10/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'tagging.apps.TaggingConfig',
     'disqus',
     'django.contrib.sites',
+    'photo.apps.PhotoConfig',
 ]
 
 DISQUS_WEBSITE_SHORTNAME = 'tellopython'
@@ -125,6 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 

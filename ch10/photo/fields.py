ffa1713971fd93cd69619c6cb37984d9b4c49bce
Lee Jin Woo <jwoo619@naver.com>
2017-11-21 23:06:46 +0900

    ch10
    add album function

--- ch10/photo/fields.py
+++ ch10/photo/fields.py
@@ -2,7 +2,7 @@ from django.db.models.fields.files import ImageField, ImageFieldFile
 from PIL import Image
 import os
 
-def __add_thumb(s):
+def _add_thumb(s):
 	parts = s.split(".")
 	parts.insert(-1,"thumb")
 	if parts[-1].lower() not in ['jpeg','jpg']:
@@ -11,20 +11,20 @@ def __add_thumb(s):
 
 class ThumbnailImageFieldFile(ImageFieldFile):
 	def _get_thumb_path(self):
-		return __add_thumb(self.path)
+		return _add_thumb(self.path)
 	thumb_path = property(_get_thumb_path)
 
 	def _get_thumb_url(self):
-		return __add_thumb(self.url)
+		return _add_thumb(self.url)
 	thumb_url = property(_get_thumb_url)
 
 	def save(self, name, content, save=True):
 		super(ThumbnailImageFieldFile,self).save(name, content, save)
 		img = Image.open(self.path)
 
-		size(128,128)
+		size = (128,128)
 		img.thumbnail(size, Image.ANTIALIAS)
-		background = Image.new('RGBA',size, (255,255,255,0))
+		background = Image.new('RGB',size, (255,255,255,0))
 		background.paste(img,(int((size[0] - img.size[0]) / 2), int((size[1] - img.size[1]) / 2) ))
 		background.save(self.thumb_path,'JPEG')
 
--- ch10/photo/views.py
+++ ch10/photo/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.views.generic import ListView , DetailView
+from photo.models import Album, Photo
 
 # Create your views here.
+
+class AlbumLV(ListView):
+	model = Album
+
+class AlbumDV(DetailView):
+	model = Album
+
+class PhotoDV(DetailView):
+	model = Photo

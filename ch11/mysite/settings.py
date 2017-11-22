e3c2958f6f788e53d15861d784772eed385dcd42
Lee Jin Woo <jwoo619@naver.com>
2017-11-22 23:34:05 +0900

    ch11
    fix setting.py , views.py ,urls.py

--- ch11/mysite/settings.py
+++ ch11/mysite/settings.py
@@ -130,6 +130,10 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+#LOGIN_URL = '/accounts/login/'
+#LOGOUT_URL = '/accounts/logout/'
+LOGIN_REDIRECT_URL = '/'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]

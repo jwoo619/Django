b40ef506c410e10574062d56631a056b7de8d887
Lee Jin Woo <jwoo619@naver.com>
2017-11-28 23:10:24 +0900

    ch12
    model, url, view fix

--- ch12/blog/models.py
+++ ch12/blog/models.py
@@ -4,6 +4,8 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.core.urlresolvers import reverse
 from tagging.fields import TagField
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 @python_2_unicode_compatible
 class Post(models.Model):
@@ -13,7 +15,8 @@ class Post(models.Model):
     content = models.TextField('CONTENT')
     create_date = models.DateTimeField('Create Date', auto_now_add=True)
     modify_date = models.DateTimeField('Modify Date', auto_now=True)
-    tag = TagField()   
+    tag = TagField() 
+    owner = models.ForeignKey(User,null=True)  
 
     class Meta:
         verbose_name = 'post'
@@ -32,4 +35,9 @@ class Post(models.Model):
 
     def get_next_post(self):
         return self.get_next_by_modify_date()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(Post, self).save(*args, **kwargs)
 
--- ch12/blog/urls.py
+++ ch12/blog/urls.py
@@ -24,6 +24,11 @@ urlpatterns = [
     url(r'^tag/(?P<tag>[^/]+(?u))/$', PostTOL.as_view(), name='tagged_object_list'),
 
     url('^search/$', SearchFormView.as_view(), name='search'),
+
+    url(r'^add/$', PostCreateView.as_view(),name="add",),
+    url(r'^change/$', PostChangeLV.as_view(),name="change",),
+    url(r'^(?P<pk>[0-9]+)/update/$', PostUpdateView.as_view(),name="update",),
+    url(r'^(?P<pk>[0-9]+)/delete/$', PostDeleteView.as_view(),name="delete",),
 ]
 
 
